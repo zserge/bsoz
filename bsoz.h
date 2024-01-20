d1e4bf201f5e04444688c3791ebdcd7c6718ac08
Serge Zaitsev <serge.zaitsev@grafana.com>
2024-01-20 15:19:12 +0100

    better formatting

--- bsoz.h
+++ bsoz.h
@@ -20,9 +20,7 @@ uint16_t rb16(struct cpu *cpu, uint16_t addr) {
   return (r8(cpu, (addr & 0xff00) | ((addr + 1) & 0xff)) << 8) | r8(cpu, addr);
 }
 
-uint8_t push(struct cpu *cpu, uint8_t b) {
-  return w8(cpu, 0x100 + cpu->s--, b);
-}
+uint8_t push(struct cpu *cpu, uint8_t b) { return w8(cpu, 0x100 + cpu->s--, b); }
 uint8_t pop(struct cpu *cpu) { return r8(cpu, 0x100 + (++cpu->s)); }
 
 void pushw(struct cpu *cpu, uint16_t w) {
@@ -51,14 +49,10 @@ void irq(struct cpu *cpu, uint16_t vec) {
 uint8_t zpg(struct cpu *cpu) { return fetch(cpu); }
 uint8_t zpx(struct cpu *cpu) { return zpg(cpu) + cpu->x; }
 uint8_t zpy(struct cpu *cpu) { return zpg(cpu) + cpu->y; }
-uint16_t abt(struct cpu *cpu) {
-  return (uint16_t)zpg(cpu) | ((uint16_t)zpg(cpu) << 8);
-}
+uint16_t abt(struct cpu *cpu) { return (uint16_t)zpg(cpu) | ((uint16_t)zpg(cpu) << 8); }
 uint16_t abx(struct cpu *cpu) { return abt(cpu) + cpu->x; }
 uint16_t aby(struct cpu *cpu) { return abt(cpu) + cpu->y; }
-uint16_t inx(struct cpu *cpu) {
-  return rb16(cpu, (fetch(cpu) + cpu->x) & 0xff);
-}
+uint16_t inx(struct cpu *cpu) { return rb16(cpu, (fetch(cpu) + cpu->x) & 0xff); }
 uint16_t iny(struct cpu *cpu) { return rb16(cpu, fetch(cpu)) + cpu->y; }
 uint16_t imm(struct cpu *cpu) { return cpu->pc++; }
 int8_t rel(struct cpu *cpu) { return (int8_t)fetch(cpu); }
@@ -90,24 +84,19 @@ uint8_t shr(struct cpu *cpu, uint8_t b, uint8_t rot) {
   cpu->f = (cpu->f & 0xfe) | (b & 1);
   return nz(cpu, r);
 }
-void shlm(struct cpu *c, uint16_t addr, uint8_t rot) {
-  w8(c, addr, shl(c, r8(c, addr), rot));
-}
-void shrm(struct cpu *c, uint16_t addr, uint8_t rot) {
-  w8(c, addr, shr(c, r8(c, addr), rot));
-}
+void shlm(struct cpu *c, uint16_t addr, uint8_t rot) { w8(c, addr, shl(c, r8(c, addr), rot)); }
+void shrm(struct cpu *c, uint16_t addr, uint8_t rot) { w8(c, addr, shr(c, r8(c, addr), rot)); }
 void flags(struct cpu *cpu, uint16_t addr) {
   uint8_t v = r8(cpu, addr);
   cpu->f = (cpu->f & 0x3d) | (0xc0 & v) | (((v & cpu->a) == 0) << 1);
 }
-uint8_t bit(struct cpu *cpu, uint8_t and, uint8_t or, uint8_t xor) {
-  return cpu->a = nz(cpu, ((cpu->a & and) | or) ^ xor);
+void bit(struct cpu *cpu, uint8_t and, uint8_t or, uint8_t xor) {
+  cpu->a = nz(cpu, ((cpu->a & and) | or) ^ xor);
 }
-uint8_t adc(struct cpu *cpu, uint16_t addr, int neg) {
+void adc(struct cpu *cpu, uint16_t addr, int neg) {
   uint8_t n = r8(cpu, addr);
   if (cpu->f & 0x08) {
     if (neg) {
-#if 1
       int nd = ((n >> 4) * 10) + (n & 0x0f) + !(cpu->f & 1);
       int val = ((cpu->a >> 4) * 10) + (cpu->a & 0x0f) - nd;
       cpu->f = cpu->f | 1;
@@ -116,9 +105,7 @@ uint8_t adc(struct cpu *cpu, uint16_t addr, int neg) {
         val += 100;
       }
       cpu->a = nz(cpu, ((val / 10) << 4) + (val % 10));
-#endif
     } else {
-#if 1
       uint8_t p1 = (cpu->a & 0xf) + (n & 0xf) + (cpu->f & 1);
       uint8_t p2 = (p1 >= 10) + (cpu->a >> 4) + (n >> 4);
       cpu->f = cpu->f & 0xfe;
@@ -128,14 +115,12 @@ uint8_t adc(struct cpu *cpu, uint16_t addr, int neg) {
         p2 -= 10;
       }
       cpu->a = nz(cpu, (p2 << 4) + p1);
-#endif
     }
-    return 0;
   } else {
     uint16_t r = cpu->a + (r8(cpu, addr) ^ (0xff * neg)) + (cpu->f & 0x01);
     int v = (((0xff * !neg) ^ (cpu->a ^ n)) & (cpu->a ^ r) & 0x80);
     cpu->f = (cpu->f & 0xbe) | ((r & 0x100) >> 8) | (v >> 1);
-    return cpu->a = nz(cpu, r & 0xff);
+    cpu->a = nz(cpu, r & 0xff);
   }
 }
 
@@ -230,21 +215,14 @@ int step(struct cpu *c) {
     case 0xe8: c->x = nz(c, c->x + 1); break; /* INX,IMP,1,2,cZidbvN */
     case 0xc8: c->y = nz(c, c->y + 1); break; /* INY,IMP,1,2,cZidbvN */
 
-    case 0xe6: inc(c, zpg(c), 1); break; /* INC,ZP,2,5,cZidbvN */
-    case 0xf6: inc(c, zpx(c), 1); break; /* INC,ZPX,2,6,cZidbvN */
-    case 0xee: inc(c, abt(c), 1); break; /* INC,ABS,3,6,cZidbvN */
-    case 0xfe:
-      inc(c, abx(c), 1);
-      break; /* INC,AX,3,7,cZidbvN */
-      /*case 0x1a: inc(c, acc(c), 1); break; [> INC,ACC,1,2,cZidbvN <]*/
-
+    case 0xe6: inc(c, zpg(c), 1); break;    /* INC,ZP,2,5,cZidbvN */
+    case 0xf6: inc(c, zpx(c), 1); break;    /* INC,ZPX,2,6,cZidbvN */
+    case 0xee: inc(c, abt(c), 1); break;    /* INC,ABS,3,6,cZidbvN */
+    case 0xfe: inc(c, abx(c), 1); break;    /* INC,AX,3,7,cZidbvN */
     case 0xc6: inc(c, zpg(c), 0xff); break; /* DEC,ZP,2,5,cZidbvN */
     case 0xd6: inc(c, zpx(c), 0xff); break; /* DEC,ZPX,2,6,cZidbvN */
     case 0xce: inc(c, abt(c), 0xff); break; /* DEC,ABS,3,6,cZidbvN */
-    case 0xde:
-      inc(c, abx(c), 0xff);
-      break; /* DEC,AX,3,7,cZidbvN */
-      /*case 0x3a: inc(c, acc(c), 0xff); break; [> DEC,ACC,1,2,cZidbvN <]*/
+    case 0xde: inc(c, abx(c), 0xff); break; /* DEC,AX,3,7,cZidbvN */
 
     case 0xc9: cmp(c, imm(c), c->a); break; /* CMP,IMM,2,2,CZidbvN */
     case 0xc5: cmp(c, zpg(c), c->a); break; /* CMP,ZP,2,3,CZidbvN */
@@ -260,11 +238,8 @@ int step(struct cpu *c) {
     case 0xec: cmp(c, abt(c), c->x); break; /* CPX,ABS,3,4,CZidbvN */
     case 0xc0: cmp(c, imm(c), c->y); break; /* CPY,IMM,2,2,CZidbvN */
     case 0xc4: cmp(c, zpg(c), c->y); break; /* CPY,ZP,2,3,CZidbvN */
-    case 0xcc:
-      cmp(c, abt(c), c->y);
-      break; /* CPY,ABS,3,4,CZidbvN */
+    case 0xcc: cmp(c, abt(c), c->y); break; /* CPY,ABS,3,4,CZidbvN */
 
-    /* Status register (flags) operations */
     case 0x18: c->f = c->f & 0xfe; break; /* CLC,IMP,1,2,Czidbvn */
     case 0xd8: c->f = c->f & 0xf7; break; /* CLD,IMP,1,2,cziDbvn */
     case 0x58: c->f = c->f & 0xfb; break; /* CLI,IMP,1,2,czIdbvn */
@@ -280,53 +255,25 @@ int step(struct cpu *c) {
 		case 0x3c: c->f = (c->f & 0x3d) | (0xc2 & r8(c, abx(c))); break; /* BIT,AX,3,4,cZidbVN */
 #endif
 
-    /* Conditional jumps */
-    case 0x90:
-      c->pc += rel(c) * ((c->f & 0x01) == 0);
-      break; /* BCC,REL,2,2/3,czidbvn */
-    case 0xB0:
-      c->pc += rel(c) * ((c->f & 0x01) != 0);
-      break; /* BCS,REL,2,2/3,czidbvn */
-    case 0xF0:
-      c->pc += rel(c) * ((c->f & 0x02) != 0);
-      break; /* BEQ,REL,2,2/3,czidbvn */
-    case 0x30:
-      c->pc += rel(c) * ((c->f & 0x80) != 0);
-      break; /* BMI,REL,2,2/3,czidbvn */
-    case 0xD0:
-      c->pc += rel(c) * ((c->f & 0x02) == 0);
-      break; /* BNE,REL,2,2/3,czidbvn */
-    case 0x10:
-      c->pc += rel(c) * ((c->f & 0x80) == 0);
-      break; /* BPL,REL,2,2/3,czidbvn */
-    case 0x50:
-      c->pc += rel(c) * ((c->f & 0x40) == 0);
-      break; /* BVC,REL,2,2/3,czidbvn */
-    case 0x70:
-      c->pc += rel(c) * ((c->f & 0x40) != 0);
-      break; /* BVS,REL,2,2/3,czidbvn */
+    case 0x90: c->pc += rel(c) * ((c->f & 0x01) == 0); break; /* BCC,REL,2,2/3,czidbvn */
+    case 0xB0: c->pc += rel(c) * ((c->f & 0x01) != 0); break; /* BCS,REL,2,2/3,czidbvn */
+    case 0xF0: c->pc += rel(c) * ((c->f & 0x02) != 0); break; /* BEQ,REL,2,2/3,czidbvn */
+    case 0x30: c->pc += rel(c) * ((c->f & 0x80) != 0); break; /* BMI,REL,2,2/3,czidbvn */
+    case 0xD0: c->pc += rel(c) * ((c->f & 0x02) == 0); break; /* BNE,REL,2,2/3,czidbvn */
+    case 0x10: c->pc += rel(c) * ((c->f & 0x80) == 0); break; /* BPL,REL,2,2/3,czidbvn */
+    case 0x50: c->pc += rel(c) * ((c->f & 0x40) == 0); break; /* BVC,REL,2,2/3,czidbvn */
+    case 0x70: c->pc += rel(c) * ((c->f & 0x40) != 0); break; /* BVS,REL,2,2/3,czidbvn */
 
-    /* Other jumps */
-    case 0x4c: c->pc = abt(c); break; /* JMP,ABS,3,3,czidbvn */
-    case 0x6c:
-      c->pc = rb16(c, abt(c));
-      break; /* JMP,IND,3,5,czidbvn */
-    /*case 0x7c: break; [> JMP,AX,3,6,czidbvn <]*/
-    case 0x20:
-      pushw(c, c->pc + 1);
-      c->pc = abt(c);
-      break; /* JSR,ABS,3,6,czidbvn */
+    case 0x4c: c->pc = abt(c); break;                      /* JMP,ABS,3,3,czidbvn */
+    case 0x6c: c->pc = rb16(c, abt(c)); break;             /* JMP,IND,3,5,czidbvn */
+    case 0x20: pushw(c, c->pc + 1), c->pc = abt(c); break; /* JSR,ABS,3,6,czidbvn */
 
-    /* Stack operations + returns */
-    case 0x48: push(c, c->a); break;        /* PHA,IMP,1,3,czidbvn */
-    case 0x68: c->a = nz(c, pop(c)); break; /* PLA,IMP,1,4,cZidbvN */
-    case 0x08: push(c, c->f | 0x30); break; /* PHP,IMP,1,3,czidbvn */
-    case 0x28: c->f = pop(c) | 0x20; break; /* PLP,IMP,1,4,CZIDBVN */
-    case 0x40:
-      c->f = pop(c) | 0x20;
-      c->pc = popw(c);
-      break;                               /* RTI,IMP,1,6,czidbvn */
-    case 0x60: c->pc = popw(c) + 1; break; /* RTS,IMP,1,6,czidbvn */
+    case 0x48: push(c, c->a); break;                         /* PHA,IMP,1,3,czidbvn */
+    case 0x68: c->a = nz(c, pop(c)); break;                  /* PLA,IMP,1,4,cZidbvN */
+    case 0x08: push(c, c->f | 0x30); break;                  /* PHP,IMP,1,3,czidbvn */
+    case 0x28: c->f = pop(c) | 0x20; break;                  /* PLP,IMP,1,4,CZIDBVN */
+    case 0x40: c->f = pop(c) | 0x20, c->pc = popw(c); break; /* RTI,IMP,1,6,czidbvn */
+    case 0x60: c->pc = popw(c) + 1; break;                   /* RTS,IMP,1,6,czidbvn */
 
     case 0xaa: c->x = nz(c, c->a); break; /* TAX,IMP,1,2,cZidbvN */
     case 0x8a: c->a = nz(c, c->x); break; /* TXA,IMP,1,2,cZidbvN */
